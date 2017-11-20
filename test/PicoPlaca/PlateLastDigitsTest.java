2081dc3052b83c3caec71659a3adac0159cd4a5d
hecey <hecey@hotmail.com>
2017-11-20 17:00:04 -0500

    Added Test For PlateLastDigits

---
+++ test/PicoPlaca/PlateLastDigitsTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PicoPlaca;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author katrina
+ */
+public class PlateLastDigitsTest {
+    
+    public PlateLastDigitsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of extractLastDigits method, of class PlateLastDigits.
+     */
+    @Test
+    public void testExtractLastDigits() {
+        System.out.println("extractLastDigits");
+        String licensePlateNumber = "ABC-1234";
+        int numberOfDigits = 1;
+        int expResult = 4;
+        int result = PlateLastDigits.extractLastDigits(licensePlateNumber, numberOfDigits);
+        assertEquals("Expeted result is 4", expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
