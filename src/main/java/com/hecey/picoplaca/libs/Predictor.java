6775dd0d3011b30b1c73c3fcf2b0425cba565257
hecey <hecey@hotmail.com>
2017-12-24 15:44:53 -0500

    Adding Emun Days, LastNumbersByDay. Full Code Coverage

--- src/main/java/com/hecey/picoplaca/libs/Predictor.java
+++ src/main/java/com/hecey/picoplaca/libs/Predictor.java
@@ -17,25 +17,20 @@ public class Predictor {
     //Ecuador have three letters and three or four numbers ranging from 000 to 9999,
     //for serial formats of ABC-123 and ABC-1234.
     //Date expected format dd-mm-yyyy
-    
     private String licensePlateNumber;
     private String date;
     private String time;
     private int licenseLastNumber;
-    
-    
+    private ILicenseLastNumberByDay licenseLastNumberByDay;
+
+    public void setLicenseLastNumberByDay(ILicenseLastNumberByDay licenseLastNumberByDay) {
+        this.licenseLastNumberByDay = licenseLastNumberByDay;
+    }
 
     public Predictor() {
         this.licensePlateNumber = "";
         this.date = "";
         this.time = "";
-    }
-
-    public Predictor(String licensePlateNumber, String date, String time) {
-        this.licensePlateNumber = licensePlateNumber;
-        this.date = date;
-        this.time = time;
-        
     }
 
     public String getLicensePlateNumber() {
@@ -50,62 +45,30 @@ public class Predictor {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getTime() {
         return time;
     }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
-
     //Returns whether or not that car can be on the road.
-    public boolean canBeOnTheRoad(Schedule schedule, LastDigits lastDigits, DateOf dateOf) throws DOException{
-        
+    public boolean canBeOnTheRoad(ISchedule schedule, ILastDigits lastDigits, IDate dateOf) throws DOException {
+
         licenseLastNumber = lastDigits.extractLastDigits(licensePlateNumber, 1);
         int dayOfTheWeek = dateOf.getDayOfTheWeek(date);
-        
-        boolean insideRestrictedTime= schedule.insideRestrictedSchedule(time);
-        
-        if(insideRestrictedTime)
-            return !isRestrictedDayByLicenseNumber(dayOfTheWeek,licenseLastNumber);
-        
+
+        boolean insideRestrictedTime = schedule.insideRestrictedSchedule(time);
+
+        if (insideRestrictedTime) {
+            return !isRestrictedDayByLicenseNumber(dayOfTheWeek, licenseLastNumber);
+        }
+
         return true;
     }
 
     private boolean isRestrictedDayByLicenseNumber(int dayOfTheWeek, int licenseLastNumber) {
-        switch(dayOfTheWeek){
-            case 2:
-                //Monday Plates numbers 1 y 2
-                if (licenseLastNumber ==1 || licenseLastNumber ==2)
-                    return true;
-                break;
-            case 3:
-                //Tuesday Plates numbers 3 y 4
-                if (licenseLastNumber ==3 || licenseLastNumber ==4)
-                    return true;
-                break;
-            case 4:
-                //Thursday Plates numbers 5 y 6
-                if (licenseLastNumber ==5 || licenseLastNumber ==6)
-                    return true;
-                break;
-            case 5:
-                //Wednesday Plates numbers 7 y 8
-                if (licenseLastNumber ==7 || licenseLastNumber ==8)
-                    return true;
-                break;
-            case 6:
-                //Friday Plates numbers 9 y 0
-                if (licenseLastNumber ==9 || licenseLastNumber ==0)
-                    return true;
-                break;
 
-                
-        }
-        return false;
+        return licenseLastNumberByDay.isRestricted(dayOfTheWeek, licenseLastNumber);
+
     }
+
+   
 }
